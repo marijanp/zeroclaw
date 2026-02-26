df648a38670b8f9907f4e15628340c779d23f721
Marijan Petričević <marijan.petricevic94@gmail.com>
2026-02-26 09:41:21 +0100

    nix: remove reference to the Rust toolchain from the runtime dependencies

--- overlay.nix
+++ overlay.nix
@@ -2,19 +2,12 @@ final: prev: {
   zeroclaw-web = final.callPackage ./web/package.nix { };
 
   zeroclaw = final.callPackage ./package.nix {
-    rustPlatform =
-      let
-        rustToolchain = final.fenix.stable.withComponents [
-          "cargo"
-          "clippy"
-          "rust-src"
-          "rustc"
-          "rustfmt"
-        ];
-      in
-      final.makeRustPlatform {
-        cargo = rustToolchain;
-        rustc = rustToolchain;
-      };
+    rustToolchain = final.fenix.stable.withComponents [
+      "cargo"
+      "clippy"
+      "rust-src"
+      "rustc"
+      "rustfmt"
+    ];
   };
 }
--- package.nix
+++ package.nix
@@ -1,8 +1,16 @@
 {
-  rustPlatform,
+  makeRustPlatform,
+  rustToolchain,
   lib,
   zeroclaw-web,
+  removeReferencesTo,
 }:
+let
+  rustPlatform = makeRustPlatform {
+    cargo = rustToolchain;
+    rustc = rustToolchain;
+  };
+in
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "zeroclaw";
   version = "0.1.7";
@@ -34,7 +42,17 @@ rustPlatform.buildRustPackage (finalAttrs: {
 
   cargoLock.lockFile = ./Cargo.lock;
 
+  nativeBuildInputs = [
+    removeReferencesTo
+  ];
+
   # Since tests run in the official pipeline, no need to run them in the Nix sandbox.
   # Can be changed by consumers using `overrideAttrs` on this package.
   doCheck = false;
+
+  # Some dependency causes Nix to detect the Rust toolchain to be a runtime dependency
+  # of zeroclaw. This manually removes any reference to the toolchain.
+  postFixup = ''
+    find "$out" -type f -exec remove-references-to -t ${rustToolchain} '{}' +
+  '';
 })
